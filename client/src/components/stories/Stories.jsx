bf2644e8a732b5552fa5913bb002ad2fe5240899
Girish Tiwale <girishmt4@gmail.com>
2022-12-05 17:47:32 -0800

    backend set up

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -5,8 +5,6 @@ import "./Stories.scss";
 //TEMP
 const imagesrc =
   "https://blog.hootsuite.com/wp-content/uploads/2022/08/Instagram-story-ideas_share-a-post-with-new-post-sticker.png";
-const propic =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyyFHOw8dO_4vTwdIEx6VeI-2U6mBz7YTQgjWVJcvT_sx3C3MMSkJ-Q8wlEeANOyTtGhY&usqp=CAU";
 
 const stories = [
   {
